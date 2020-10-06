a29efccced805fa51560ebba3fc2e1860c07b98b
gvjas <gvjasch@gmail.com>
2020-10-06 18:23:37 +0900

    feat: solution for task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let tmp;
+  while (str) {
+    for (brackets of bracketsConfig) {
+      str = str.replace(brackets.join(''), '')
+    }
+    if (str === tmp) {
+      return false;
+    }
+    tmp = str;
+  }
+  return str.length === 0;
+
 }
